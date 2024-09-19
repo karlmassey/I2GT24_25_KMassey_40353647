abc7994b8e5de34efbaefbd4bfb342793cdb8e43
Karl Massey <65904300+karlmassey@users.noreply.github.com>
2024-09-19 18:01:37 +0100

    Challenge 1 complete
    - Corrected bugs to complete challenge 1

---
+++ Create with Code Part 1 Mission 1 - Player Control/Challenge 1/Assets/Challenge 1/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 _cameraOffset = new Vector3(30, 0, 10);
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        // Ensure camera position moves with player object
+        transform.position = plane.transform.position + _cameraOffset;
+    }
+}
---
+++ Create with Code Part 1 Mission 1 - Player Control/Challenge 1/Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+{
+    private float _propellerSpeed = 1000f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Make the propeller spin at a constant speed
+        transform.Rotate(Vector3.forward * Time.deltaTime * _propellerSpeed);
+    }
+}
