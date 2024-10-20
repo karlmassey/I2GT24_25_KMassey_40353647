4dd11b3f4c64d6076d03fb5e3512319d6809f4a9
Karl Massey <65904300+karlmassey@users.noreply.github.com>
2024-10-20 19:13:22 +0100

    Easy - Vertical Player Movement Complete

--- Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -8,8 +8,11 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
 
-    public float speed = 10.0f;
-    public float xRange = 15f;
+    private float _speed = 10.0f;
+    private float _xRange = 20f;
+
+    private float _minZ = -1f;
+    private float _maxZ = 15.5f;
 
     public GameObject projectilePrefab;
 
@@ -21,18 +24,34 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (transform.position.x < -xRange)
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
-
-        if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
-
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * _speed);
+
+        if (transform.position.x < -_xRange)
+        {
+            transform.position = new Vector3(-_xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > _xRange)
+        {
+            transform.position = new Vector3(_xRange, transform.position.y, transform.position.z);
+        }
+
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * _speed);
+
+        if (transform.position.z < _minZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,
+            _minZ);
+        }
+        if (transform.position.z > _maxZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,
+            _maxZ);
+        }
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
